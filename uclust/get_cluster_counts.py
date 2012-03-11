5b4b15a7a5639930f43c1f7d8377ff3ccc1f5975
Brant Faircloth <brant.faircloth@gmail.com>
2012-03-10 17:17:18 -0800

    reformat for appropriate subprocess use

--- uclust/get_cluster_counts.py
+++ uclust/get_cluster_counts.py
@@ -5,18 +5,18 @@ Author: Brant Faircloth
 Created by Brant Faircloth on 14 December 2011 21:12 PST (-0800)
 Copyright (c) 2011 Brant C. Faircloth. All rights reserved.
 
-Description: 
+Description: Given a fasta file of sequences, cluster those reads by
+user-specific parameter and write results to outdir.
 
 """
 
 import os
 import re
-import sys
 import glob
-import shutil
 import argparse
+import subprocess
 from seqtools.fs.args import FullPaths, is_dir
-from subprocess import Popen, PIPE
+
 
 import pdb
 
@@ -26,46 +26,55 @@ def get_args():
             description="""Match UCE probes to assembled
             contigs and store the data""")
     parser.add_argument(
-            'input',
+            'indir',
             action=FullPaths,
             type=is_dir,
             help="""The input directory containing fastas
             to cluster"""
         )
     parser.add_argument(
-            '--output',
-            default=os.getcwd(),
+            'outdir',
             action=FullPaths,
             type=is_dir,
             help="""The output directory to hold results""",
         )
     parser.add_argument(
             '--id',
-            help="""The % identity to use""",
-            default=0.9
+            default=0.9,
+            help="""The % identity to use"""
         )
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    #pdb.set_trace()
-    # remove prior results
     regex = re.compile('([0-9]+)\sclusters')
-    for file in glob.glob(os.path.join(args.input, "*.fasta")):
-        name = os.path.splitext(file)[0]
-        outname = os.path.split(name)[1]
-        sort = "uclust --sort {0}.fasta --output {1}.sorted.fasta 2> /dev/null"
-        cluster = "uclust --input {0}.sorted.fasta --uc {0}.sorted.results.uc --id {1}"
-        uc_sort = Popen(sort.format(name, os.path.join(args.output, outname)), shell=True, stdout=PIPE)
-        sort_stdout, sort_stderr = uc_sort.communicate()
-        uc_cluster = Popen(cluster.format(os.path.join(args.output, outname), args.id), shell=True,
-                stdout=PIPE, stderr=PIPE)
-        cluster_stdout, cluster_stderr = uc_cluster.communicate()
+    for fasta in glob.glob(os.path.join(args.indir, "*.fasta")):
+        name = os.path.splitext(fasta)[0]
+        outname = os.path.join(args.outdir, os.path.split(name)[1])
+        sort_cmd = [
+                "uclust",
+                "--sort",
+                fasta,
+                "--output",
+                "{0}.sorted.fasta".format(outname)
+            ]
+        sort = subprocess.Popen(sort_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        sort_stdout, sort_stderr = sort.communicate()
+        cluster_cmd = [
+                "uclust",
+                "--input",
+                "{0}.sorted.fasta".format(outname),
+                "--uc",
+                "{0}.sorted.results.uc".format(outname),
+                "--id",
+                args.id
+            ]
+        cluster = subprocess.Popen(cluster_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cluster_stdout, cluster_stderr = cluster.communicate()
         clusters = regex.search(cluster_stderr)
         cluster_count = int(clusters.groups()[0])
-        print os.path.split(file)[1], cluster_count
-        #pdb.set_trace()
+        print os.path.split(fasta)[1], cluster_count
 
 
 if __name__ == '__main__':
