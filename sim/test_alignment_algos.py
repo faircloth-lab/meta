8424f495f36b3b3f882a9b5a225022f8ca21e1d7
Brant Faircloth <brant@faircloth-lab.org>
2014-07-23 12:23:25 -0700

    updating alignment algo test

--- sim/test_alignment_algos.py
+++ sim/test_alignment_algos.py
@@ -16,6 +16,7 @@ import re
 import sys
 import copy
 import tempfile
+import argparse
 import subprocess
 import multiprocessing
 from Bio import SeqIO
@@ -23,29 +24,136 @@ from collections import defaultdict
 
 import pdb
 
-def run_swipe(work):
-    rv = None
-    sequence, regex = work
-    slcs = range(10, len(sequence.seq), 10)
-    slcs.append(len(sequence.seq))
-    for slc in reversed(slcs):
-        # write sequence out to tempfile
-        handle, file = tempfile.mkstemp(suffix='.fasta')
-        os.write(handle, sequence[:slc].format('fasta'))
-        os.close(handle)
-        # create output tempfile
-        handle, output = tempfile.mkstemp(suffix='.swipe')
-        os.close(handle)
-        # run alignment of sequence against database
-        # ~/src/swipe-2.0.4/swipe -d bci.fasta -i test.fasta -p 0 -M BLOSUM80 -G 100 > out.results
-        cmd = [
+
+def get_args():
+    """Get arguments from CLI"""
+    parser = argparse.ArgumentParser(
+            description="""Test difference alignment programs""")
+    parser.add_argument(
+            "fasta",
+            help="""The fasta file to use"""
+        )
+    parser.add_argument(
+            "db",
+            help="""The database to use"""
+        )
+    parser.add_argument(
+            "algo",
+            help="""The algorithm to pass"""
+        )
+    parser.add_argument(
+            "output",
+            help="""The output file name"""
+        )
+    parser.add_argument(
+            "--taxa",
+            type=int,
+            help="""The number of taxa to screen""",
+        )
+    parser.add_argument(
+            "--cores",
+            type=int,
+            help="""The number of compute cores to use""",
+        )
+    parser.add_argument(
+            "--five-prime",
+            action="store_true",
+            default=False,
+            help="""Trim sequence from 5' => 3' instead of reverse""",
+        )
+    return parser.parse_args()
+
+
+def get_swipe_cmd(file, db, output):
+    # ~/src/swipe-2.0.4/swipe -d bci.fasta -i test.fasta -p 0 -M BLOSUM80 -G 100 > out.results
+    cmd = [
             '/home/bcf/src/swipe-2.0.4/swipe',
-            '-d', 'bci.fasta',
+            '-d', db,
             '-i', file,
             '-p', '0',
             '-M', 'BLOSUM80',
             '-G', '100',
-            '-o', output]
+            '-o', output
+        ]
+    return cmd
+
+
+def get_blast_cmd(file, db, output):
+    # blastn -db bci.fasta -query abarema-macradenia-0.fasta -task megablast -evalue 1e-50 > abarema-out.txt
+    cmd = [
+            'blastn',
+            '-db', db,
+            '-query', file,
+            '-task', 'megablast',
+            '-evalue', '1e-50',
+            '-gapopen', '3',
+            '-gapextend', '1',
+            '-out', output
+        ]
+    return cmd
+
+
+def get_blat_cmd(file, db, output):
+    # blastn -db bci.fasta -query abarema-macradenia-0.fasta -task megablast -evalue 1e-50 > abarema-out.txt
+    cmd = [
+            'blat',
+            db,
+            file,
+            '-t=dna',
+            '-q=dna',
+            '-fastMap',
+            '-out=blast',
+            output
+        ]
+    return cmd
+
+
+def get_cmd_line(algo, file, db, output):
+    # run alignment of sequence against database
+    if algo == 'swipe':
+        cmd = get_swipe_cmd(file, db, output)
+    elif algo == 'blast':
+        cmd = get_blast_cmd(file, db, output)
+    elif algo == 'blat':
+        cmd = get_blat_cmd(file, db, output)
+    return cmd
+
+
+def parse_alignment_results(regex, output):
+    container = defaultdict(list)
+    for line in open(output, 'rU'):
+        if line.startswith('>'):
+            break
+        else:
+            result = regex.search(line)
+            if result:
+                grp = result.groups()
+                container[int(grp[1])].append(grp)
+    return container
+
+
+def run_algo(work):
+    rv = None
+    sequence, regex, algo, five_prime, db = work
+    if five_prime:
+        slcs = range(0, len(sequence.seq), 10)
+    else:
+        slcs = range(10, len(sequence.seq), 10)
+        slcs.append(len(sequence.seq))
+        slcs = reversed(slcs)
+    for slc in slcs:
+        # write sequence out to tempfile
+        handle, file = tempfile.mkstemp(suffix='.fasta')
+        if five_prime:
+            os.write(handle, sequence[slc:].format('fasta'))
+        else:
+            os.write(handle, sequence[:slc].format('fasta'))
+        os.close(handle)
+        # create output tempfile
+        handle, output = tempfile.mkstemp(suffix='.swipe')
+        os.close(handle)
+        # get the appropriate cmd
+        cmd = get_cmd_line(algo, file, db, output)
         process = subprocess.Popen(
                 cmd,
                 stdout=subprocess.PIPE,
@@ -53,15 +161,7 @@ def run_swipe(work):
             )
         stdout, stderr = process.communicate()
         # parse the alignment results
-        container = defaultdict(list)
-        for line in open(output, 'rU'):
-            if line.startswith('>'):
-                break
-            else:
-                result = regex.search(line)
-                if result:
-                    grp = result.groups()
-                    container[int(grp[2])].append(grp)
+        container = parse_alignment_results(regex, output)
         # remove the temp files
         for f in [file, output]:
             os.remove(f)
@@ -70,11 +170,32 @@ def run_swipe(work):
             best_hits = container[max(container.keys())]
             best_taxa = set(['_'.join(taxon[0].split('_')[:2]) for taxon in best_hits])
             best_taxa_with_ids = set([taxon[0] for taxon in best_hits])
-            if slc == len(sequence.seq):
+            if five_prime and slc == 0:
                 first_taxa = copy.deepcopy(best_taxa)
+            elif not five_prime and slc == len(sequence.seq):
+                first_taxa = copy.deepcopy(best_taxa)
+        # if we reach the maximum, or the results change, or we don't get results
+        if five_prime and (slc == max(slcs) or (best_taxa != first_taxa) or not container):
+            rv = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(
+                    sequence.id,
+                    len(sequence),
+                    len(sequence) - previous_slc,
+                    previous_max, previous_runner_up,
+                    ', '.join(previous_taxa),
+                    ', '.join(best_taxa_with_ids)
+                )
+            break
         # if we reach the minimum, or the results change, or we don't get results
-        if slc == 10 or (best_taxa != first_taxa) or not container:
-            rv = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n".format(sequence.id, len(sequence), previous_slc, previous_max, previous_runner_up, ', '.join(previous_taxa))
+        elif not five_prime and (slc == 10 or (best_taxa != first_taxa) or not container):
+            rv = "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\n".format(
+                    sequence.id,
+                    len(sequence),
+                    previous_slc,
+                    previous_max,
+                    previous_runner_up,
+                    ', '.join(previous_taxa),
+                    ', '.join(best_taxa_with_ids)
+                )
             break
         previous_taxa = copy.deepcopy(best_taxa_with_ids)
         previous_slc = slc
@@ -91,20 +212,37 @@ def run_swipe(work):
         # if all else fails
         return "{0}\t{1}\tNone\tNone\tNone\n".format(sequence.id, len(sequence))
 
+
+def get_algo_regex(algo):
+    if algo == 'swipe':
+        # compile regular expression to pick up summary info line once
+        regex = re.compile("^gnl\|(?:.*)\|[0-9]+\s([A-Za-z0-9_]+)\s+(?:.*)\s+([0-9]+)\s+(.*)$")
+    elif algo == 'blast':
+        regex = re.compile("^\s+([A-Za-z0-9_]+)\s+([0-9]+)\s+(.*)$")
+    elif algo == 'blat':
+        regex = re.compile("^([A-Za-z0-9_]+)\s+([0-9]+)\s+(.*)$")
+    return regex
+
+
 def main():
     """main loop"""
-    # compile regular expression to pick up summary info line once
-    regex = re.compile("^gnl\|(?:.*)\|[0-9]+\s([A-Za-z0-9_]+)\s+(.*)\s+([0-9]+)\s+(.*)$")
-    work = [(seq, regex) for seq in SeqIO.parse(open('bci.fasta', 'rU'), 'fasta')]
-    pool = multiprocessing.Pool(12)
+    args = get_args()
+    regex = get_algo_regex(args.algo)
+    work = [(seq, regex, args.algo, args.five_prime, args.db) for seq in SeqIO.parse(open(args.fasta, 'rU'), 'fasta')]
+    if args.taxa:
+        work = work[:args.taxa]
     sys.stdout.write("Running")
     sys.stdout.flush()
-    results = pool.map(run_swipe, work)
-    outp = open('matches.tdt', 'w')
+    if args.cores > 1:
+        pool = multiprocessing.Pool(args.cores)
+        results = pool.map(run_algo, work)
+    else:
+        results = map(run_algo, work)
+    outp = open(args.output, 'w')
     for result in results:
         outp.write(result)
     outp.close()
-    
+
 
 if __name__ == '__main__':
     main()
